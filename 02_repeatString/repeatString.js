47762f074e4dbb284fd39ab924e6d0f25954b789
sarah-abraham <sarahabraham2014@gmail.com>
2024-06-12 09:47:00 +0530

    Completed exercise 2 where a string had to be repeated n number of times. The string to be repeated and n were provided as input

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, n) {
+    let i;
+    let stringWithRepeatedWord = "";
+    if(n<0){
+       return 'ERROR' 
+    }
+    for(i = 0; i<n; i++){
+        stringWithRepeatedWord += word;
+    }
+    return stringWithRepeatedWord
 };
 
 // Do not edit below this line
