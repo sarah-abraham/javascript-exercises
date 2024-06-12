55fb1b1a6001b7580c974dd1432186d4ee99f03b
sarah-abraham <sarahabraham2014@gmail.com>
2024-06-12 10:49:03 +0530

    Completed exercise 4 where givenelements had to be removed from the given array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elementsToRemove) {
+    elementsToRemove.forEach((element) => {
+        while(arr.includes(element) && arr[arr.indexOf(element)]===element){
+            arr.splice(arr.indexOf(element), 1);
+        }
+    })
+    return arr;
 };
 
 // Do not edit below this line
