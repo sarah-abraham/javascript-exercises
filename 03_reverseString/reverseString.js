a60be6efa26096c45a52059adda10f3e33b8dc7b
sarah-abraham <sarahabraham2014@gmail.com>
2024-06-12 10:24:32 +0530

    Completed exercise 3 where a given string had to be reversed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(sentence) {
+    let characterArray = sentence.split("");
+    let n = characterArray.length;
+    let reversedCharacterArray = [];
+    let i = 0, j = n-1;
+    while(i<n && j>=0){
+        reversedCharacterArray[j] = characterArray[i];
+        j--;
+        i++;
+    }
+    let reversedStringResult = reversedCharacterArray.join("");
+    return reversedStringResult
 };
 
 // Do not edit below this line
