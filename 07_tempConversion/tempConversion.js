56727269f0eea67b190aeed6f4abbb7e6abee591
sarah-abraham <sarahabraham2014@gmail.com>
2024-06-12 11:27:51 +0530

    Completed exercise 7 where temperature in celsius had to be converted to fahrenheit and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemperature) {
+  let result =  Math.ceil(((fahrenheitTemperature - 32)*5/9)*10)/10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemperature) {
+  let result = Math.ceil(((celsiusTemperature*9/5)+32)*10)/10;
+  return result;
 };
 
 // Do not edit below this line
