b63eaad12234a72f949a33c3ee46dc771f6d38bb
sarah-abraham <sarahabraham2014@gmail.com>
2024-06-12 11:03:59 +0530

    Completed exercise 5 where the sum of all numbers between two given parameters had to be found

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    let i;
+    let sum = 0;
+    let highNumber, lowNumber;
+    if(first>=0 && second>=0 && typeof first =='number' && typeof second=='number'){ 
+        if(first>second){
+            highNumber = first;
+            lowNumber = second;
+        }
+        else{
+            highNumber = second;
+            lowNumber = first;
+        }
+        for(i=lowNumber; i<=highNumber; i++){
+            sum += i;
+        }
+        return sum;
+    }
+    else{
+        return 'ERROR';
+    }
 
 };
 
