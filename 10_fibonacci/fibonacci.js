7220498cb1854d481f6c79feb4683fa6d013f415
sarah-abraham <sarahabraham2014@gmail.com>
2024-06-14 12:17:39 +0530

    Completed exercise 10 in which the fibonacci series value of a given number had to be found

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(typeof num!='number'){
+        num = Number(num);
+    }
+    if(num === 0){
+        return 0;
+    }
+    else if(num < 0){
+        return 'OOPS';
+    }
+
+    let t1 = 1;
+    let t2 = 0;
+    let i;
+    for(i=2; i<=num; i++){
+        let current = t1 + t2;
+        t2 = t1;
+        t1 = current;
+    }
+    return t1;
 
 };
 
