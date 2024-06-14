ed56a10fba5d53969f8c3214aa991d49d22d47bb
sarah-abraham <sarahabraham2014@gmail.com>
2024-06-14 12:32:45 +0530

    Completed exercise 11 where book titles had to be returned

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let bookTitles = [];
+    for(let key in books){
+        if(!books[key].title){
+            continue;
+        }
+        bookTitles.push(books[key].title);
+    }
+    return bookTitles;
 };
 
 // Do not edit below this line
