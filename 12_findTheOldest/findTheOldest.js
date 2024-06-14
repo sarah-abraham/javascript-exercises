88c02694fc8bb73b8e318ae0038d278f9f4cdfa8
sarah-abraham <sarahabraham2014@gmail.com>
2024-06-14 12:56:22 +0530

    Completed exercise 12 where the person who is the oldest in the array of objects had to be returned

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let sortedAges = arr.sort((a,b) => {
+        if(!a.yearOfDeath){
+            a.yearOfDeath = (new Date()).getFullYear(); 
+        }
+        else if(!b.yearOfDeath){
+            b.yearOfDeath = (new Date()).getFullYear();
+        }
 
+        return a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth ? 1 : -1;
+
+    })
+
+    return sortedAges[(sortedAges.length)-1];
 };
 
 // Do not edit below this line
