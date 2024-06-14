9d6180c2b98f5eacae707c658797f26dfcd2a121
sarah-abraham <sarahabraham2014@gmail.com>
2024-06-14 11:57:04 +0530

    Completed exercise 9 where it had to be checked whether a string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const alphanumerical = "abcdefghijklmnopqrstuvqxyz0123456789";
+    let wordArr = word.toLowerCase().split("").filter((character) => alphanumerical.includes(character));
+    let reversedArr = word.toLowerCase().split("").reverse().filter((character) => alphanumerical.includes(character));
+    let i = 0;
+    for(i = 0; i<wordArr.length; i++){
+        if(wordArr[i] != reversedArr[i]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
