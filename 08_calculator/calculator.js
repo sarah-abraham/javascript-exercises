b4631f9e151457cba39c498d7d5147ba522a4c38
sarah-abraham <sarahabraham2014@gmail.com>
2024-06-14 11:16:10 +0530

    Completed exercise 8 where various math operations had to be performed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a>b ? a-b : b-a;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = arr.reduce((total, element) => {return total + element}, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = arr.reduce((total, element) => {return total * element}, 1);
+  if(product === 1){
+    return 0;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base,upper) {
+	return Math.pow(base,upper);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num===0 || num===1){
+    return 1;
+  }
+  else if(num==2){
+    return 2;
+  }
+  else{
+    let i;
+    let prod = 1;
+    for(i = 2; i<=num; i++){
+        prod = prod*i;
+    }
+    return prod;
+  }
 };
 
 // Do not edit below this line
